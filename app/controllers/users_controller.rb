03131202eb034e2dc899e846dfbc2fc9cf815889
mike092 <mr@gmail.com>
2015-03-09 17:45:04 +0100

    added profile page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  expose(:user)
+  expose(:reviews)
+  def index
+  end
+
+  def show
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
+  get 'users/index'
+
+  get 'users/show'
+
   devise_for :users
+  resources :users, only: [:show]
+  get 'users/' => 'users#index'
 
   resources :categories do
     resources :products do
